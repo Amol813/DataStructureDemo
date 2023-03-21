6da1af3685c5ff5244dd842661396738db29444b
unknown <amoljadhav>
2023-03-21 13:13:59 +0530

    [Amol]Add:WelcomeToTheDatastructureProblem

---
+++ DataStructureDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
